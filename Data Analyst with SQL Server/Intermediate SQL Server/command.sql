708b4c61292451ca2c9eee4c474d8fc63e1bab7a
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-03-27 15:41:02 +0600

    Update command.sql

--- Data Analyst with SQL Server/Intermediate SQL Server/command.sql
+++ Data Analyst with SQL Server/Intermediate SQL Server/command.sql
@@ -107,8 +107,50 @@ FROM Shipments
 
 --                                                      Chapter 3: Processing Data in SQL Server
 
+
+DECLARE @counter INT
+SET @counter = 20
+
+-- Create a loop
+WHILE @counter<30
+
+-- Loop code starting point
+BEGIN
+	SELECT @counter = @counter + 1
+-- Loop finish
+END
+
+-- Check the value of the variable
+SELECT @counter
+
+ --Return MaxGlucose from the derived table.
+ --Join the derived table to the main query on Age.
+SELECT a.RecordId, a.Age, a.BloodGlucoseRandom,
+-- Select maximum glucose value (use colname from derived table)    
+       b.MaxGlucose
+FROM Kidney a
+-- Join to derived table
+JOIN (SELECT Age, MAX(BloodGlucoseRandom) AS MaxGlucose FROM Kidney GROUP BY Age) b
+-- Join on Age
+ON a.Age=b.Age
+
+
+/* Create a derived table
+returning Age and MaxBloodPressure; the latter is the maximum of BloodPressure.
+is taken from the kidney table.
+is grouped by Age.
+Join the derived table to the main query on
+blood pressure equal to max blood pressure.
+age. */
+
+SELECT *
+FROM Kidney a
+-- JOIN and create the derived table
+JOIN (SELECT Age, MAX(BloodPressure) AS MaxBloodPressure FROM Kidney GROUP BY Age) b
+-- JOIN on BloodPressure equal to MaxBloodPressure
+ON a.BloodPressure = b.MaxBloodPressure
+-- Join on Age
+AND a.Age = b.Age
+
 --
-
-
-
 
