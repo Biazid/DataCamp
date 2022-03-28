ec907d2229589dd761b11cbf13616df50de8b3e9
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-03-28 23:51:14 +0600

    Update Commands.sql

--- Data Analyst with SQL Server/4. Time Series Analysis in SQL Server/Commands.sql
+++ Data Analyst with SQL Server/4. Time Series Analysis in SQL Server/Commands.sql
@@ -86,6 +86,18 @@ SELECT
 	DATEDIFF(HOUR, @TwoDaysAgo, @PostLeapDay) AS HoursDifference,
 	DATEDIFF(MINUTE, @TwoDaysAgo, @PostLeapDay) AS MinutesDifference;
   
+--Use DATEADD() and DATEDIFF() in conjunction with date parts to round down our time to the day, hour, and minute.
+DECLARE
+	@SomeTime DATETIME2(7) = '2018-06-14 16:29:36.2248991';
+-- Fill in the appropriate functions and date parts
+SELECT
+	DATEADD(DAY, DATEDIFF(DAY, 0, @SomeTime), 0) AS RoundedToDay,
+	DATEADD(HOUR, DATEDIFF(HOUR, 0, @SomeTime), 0) AS RoundedToHour,
+	DATEADD(MINUTE, DATEDIFF(MINUTE, 0, @SomeTime), 0) AS RoundedToMinute;
+	
+RoundedToDay		RoundedToHour		RoundedToMinute
+2018-06-14 00:00:00	2018-06-14 16:00:00	2018-06-14 16:29:00
+
 --
 
 
