6ca0e9a62bb13fb1847d5fe33759029df98eb5c2
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-03-29 13:06:38 +0600

    Update Commands.sql

--- Data Analyst with SQL Server/4. Time Series Analysis in SQL Server/Commands.sql
+++ Data Analyst with SQL Server/4. Time Series Analysis in SQL Server/Commands.sql
@@ -156,8 +156,44 @@ SELECT
 	-- Fill in the locale for Japan
 	FORMAT(@Python3ReleaseDate, 'd', 'jp-JP') AS JP_d,
 	FORMAT(@Python3ReleaseDate, 'd', 'zh-cn') AS CN_d;
-	
+--Output:		
+US_d		DE_d		JP_d		CN_d
+12/3/2008	03.12.2008	12/03/2008	2008/12/3	
+
 --Use the 'D' format parameter (this is case sensitive!) to build long dates. Also, fill in the culture for Indonesia, which in the .NET framework is id-ID.
+DECLARE
+	@Python3ReleaseDate DATETIME2(3) = '2008-12-03 19:45:00.033';
+SELECT
+	-- Fill in the format parameter
+	FORMAT(@Python3ReleaseDate, 'D', 'en-US') AS US_D,
+	FORMAT(@Python3ReleaseDate, 'D', 'de-DE') AS DE_D,
+	-- Fill in the locale for Indonesia
+	FORMAT(@Python3ReleaseDate,	'D', 'id-ID') AS ID_D,
+	FORMAT(@Python3ReleaseDate, 'D', 'zh-cn') AS CN_D;
+	
+US_D				DE_D				ID_D			CN_D
+Wednesday, December 3, 2008	Mittwoch, 3. Dezember 2008	Rabu, 03 Desember 2008	2008年12月3日
+
+--Fill in the custom format strings needed to generate the results in preceding comments. Use date parts such as yyyy, MM, MMM, and dd. 
+--Capitalization is important for the FORMAT() function! See the full list at https://bit.ly/30SGA5a.
+DECLARE
+	@Python3ReleaseDate DATETIME2(3) = '2008-12-03 19:45:00.033';
+SELECT
+	-- 20081203
+	FORMAT(@Python3ReleaseDate, 'yyyyMMdd') AS F1,
+	-- 2008-12-03
+	FORMAT(@Python3ReleaseDate, 'yyyy-MM-dd') AS F2,
+	-- Dec 03+2008 (the + is just a "+" character)
+	FORMAT(@Python3ReleaseDate, 'MMM dd+yyyy') AS F3,
+	-- 12 08 03 (month, two-digit year, day)
+	FORMAT(@Python3ReleaseDate, 'MM yy dd') AS F4,
+	-- 03 07:45 2008.00
+    -- (day hour:minute year.second)
+	FORMAT(@Python3ReleaseDate, 'dd hh:mm yyyy.ss') AS F5;
+
+
+	--Working with calendar tables
+
 
 
 
