d852273de6f281dcde32764493b32003c16772c8
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-03-29 12:13:28 +0600

    Update Commands.sql

--- Data Analyst with SQL Server/4. Time Series Analysis in SQL Server/Commands.sql
+++ Data Analyst with SQL Server/4. Time Series Analysis in SQL Server/Commands.sql
@@ -145,7 +145,19 @@ SELECT
 	CONVERT(NVARCHAR(30), @CubsWinWorldSeries, 101) AS US_mdyyyy;
 
 
---
+--Fill in the function and use the 'd' format parameter (note that this is case sensitive!) to format as short dates. Also, fill in the culture for Japan, 
+--which in the .NET framework is jp-JP (this is not case sensitive).
+DECLARE
+	@Python3ReleaseDate DATETIME2(3) = '2008-12-03 19:45:00.033';
+SELECT
+	-- Fill in the function call and format parameter
+	FORMAT(@Python3ReleaseDate, 'd', 'en-US') AS US_d,
+	FORMAT(@Python3ReleaseDate, 'd', 'de-DE') AS DE_d,
+	-- Fill in the locale for Japan
+	FORMAT(@Python3ReleaseDate, 'd', 'jp-JP') AS JP_d,
+	FORMAT(@Python3ReleaseDate, 'd', 'zh-cn') AS CN_d;
+	
+--Use the 'D' format parameter (this is case sensitive!) to build long dates. Also, fill in the culture for Indonesia, which in the .NET framework is id-ID.
 
 
 
