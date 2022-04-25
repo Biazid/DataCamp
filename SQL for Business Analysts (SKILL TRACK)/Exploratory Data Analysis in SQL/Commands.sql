1772dbdbb3cdee5c0ba585a2a5f82baf08d8cb6f
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-25 12:24:33 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -308,12 +308,40 @@ SELECT lower, upper, count(question_count)
  ORDER BY lower;
  
  
- --		
+ --		Correlation
+ 
+ --Compute the correlation between revenues and profits.
+--Compute the correlation between revenues and assets.
+--Compute the correlation between revenues and equity.
+
+-- Correlation between revenues and profit
+SELECT corr(revenues,profits) AS rev_profits,
+	   -- Correlation between revenues and assets
+       corr(revenues,assets) AS rev_assets,
+       -- Correlation between revenues and equity
+       corr(revenues,equity) AS rev_equity 
+  FROM fortune500;
+  
+  --		Mean and Median
+
+-- Select the mean and median of assets.
+--Group by sector.
+--Order the results by the mean.
+
+-- What groups are you computing statistics by?
+SELECT sector,
+       -- Select the mean of assets with the avg function
+       avg(assets) AS mean,
+       -- Select the median
+       percentile_disc(0.5) WITHIN GROUP (ORDER BY assets) AS median
+  FROM fortune500
+ -- Computing statistics for each what?
+ GROUP BY sector
+ -- Order results by a value of interest
+ ORDER BY mean;
 
 
-
-
-
+--		
 
 
 
