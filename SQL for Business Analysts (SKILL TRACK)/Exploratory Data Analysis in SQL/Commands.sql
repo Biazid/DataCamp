4fbfebae990045b32e9faa8f2f6e09379c22189a
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-25 14:52:38 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -341,7 +341,163 @@ SELECT sector,
  ORDER BY mean;
 
 
---		
+--		Create a temp table
+---- To clear table if it already exists;
+-- fill in name of temp table
+DROP TABLE IF EXISTS profit80;
+-- Create the temporary table
+CREATE TEMP TABLE profit80 AS 
+  -- Select the two columns you need; alias as needed
+  SELECT sector,
+         percentile_disc(0.8) WITHIN GROUP (ORDER BY profits) AS pct80
+    -- What table are you getting the data from?
+    FROM fortune500
+   -- What do you need to group by?
+   GROUP BY sector;
+SELECT * 
+  FROM profit80;
+  
+  
+  --2 -- DROP TABLE IF EXISTS profit80;
+CREATE TEMP TABLE profit80 AS
+  SELECT sector, 
+         percentile_disc(0.8) WITHIN GROUP (ORDER BY profits) AS pct80
+    FROM fortune500 
+   GROUP BY sector;
+-- Select columns, aliasing as needed
+SELECT title, fortune500.sector, 
+       profits, profits/pct80 AS ratio
+-- What tables do you need to join?  
+  FROM fortune500 
+       LEFT JOIN profit80
+-- How are the tables joined?
+       ON fortune500.sector=profit80.sector
+-- What rows do you want to select?
+ WHERE profits>pct80;
+
+
+---		Create a temp table to simplify a query
+--First, create a temporary table called startdates with each tag and the min() date for the tag in stackoverflow.
+-- To clear table if it already exists
+DROP TABLE IF EXISTS startdates;
+-- Create temp table syntax
+CREATE temp table startdates AS
+-- Compute the minimum date for each what?
+SELECT tag,
+       min(date) AS mindate
+  FROM stackoverflow
+ -- What do you need to compute the min date for each tag?
+ Group by tag;
+ -- Look at the table you created
+ SELECT * 
+   FROM startdates;
+   
+   
+   --2 --Join startdates to stackoverflow twice using different table aliases.
+--For each tag, select mindate, question_count on the mindate, and question_count on 2018-09-25 (the max date).
+--Compute the change in question_count over time.
+DROP TABLE IF EXISTS startdates;
+CREATE TEMP TABLE startdates AS
+SELECT tag, min(date) AS mindate
+  FROM stackoverflow
+ GROUP BY tag;
+SELECT startdates.tag, 
+       mindate, 
+       -- Select question count on the min and max days
+	   so_min.question_count AS min_date_question_count,
+       so_max.question_count AS max_date_question_count,
+       -- Compute the change in question_count (max- min)
+       so_max.question_count - so_min.question_count AS change
+  FROM startdates
+       -- Join startdates to stackoverflow with alias so_min
+       INNER JOIN stackoverflow AS so_min
+          -- What needs to match between tables?
+          ON startdates.tag = so_min.tag
+         AND startdates.mindate = so_min.date
+       -- Join to stackoverflow again with alias so_max
+       INNER JOIN stackoverflow AS so_max
+       	  -- Again, what needs to match between tables?
+          ON startdates.tag = so_max.tag
+          AND so_max.date = '2018-09-25';
+	  
+	  
+	  
+	--	Insert into a temp table
+
+--Create a temp table correlations.
+--Compute the correlation between profits and each of the three variables (i.e. correlate profits with profits, profits with profits_change, etc).
+--Alias columns by the name of the variable for which the correlation with profits is being computed.
+DROP TABLE IF EXISTS correlations;
+-- Create temp table 
+Create temp table correlations AS
+-- Select each correlation
+SELECT 'profits'::varchar AS measure,
+       -- Compute correlations
+       corr(profits,profits) AS profits,
+       corr(profits,profits_change) AS profits_change,
+       corr(profits,revenues_change) AS revenues_change
+  FROM fortune500;
+
+--Insert rows into the correlations table for profits_change and revenues_change.
+DROP TABLE IF EXISTS correlations;
+CREATE TEMP TABLE correlations AS
+SELECT 'profits'::varchar AS measure,
+       corr(profits, profits) AS profits,
+       corr(profits, profits_change) AS profits_change,
+       corr(profits, revenues_change) AS revenues_change
+  FROM fortune500;
+-- Add a row for profits_change
+-- Insert into what table?
+INSERT INTO correlations
+-- Follow the pattern of the select statement above
+-- Using profits_change instead of profits
+SELECT 'profits_change'::varchar AS measure,
+       corr(profits_change, profits) AS profits,
+       corr(profits_change, profits_change) AS profits_change,
+       corr(profits_change, revenues_change) AS revenues_change
+  FROM fortune500;
+
+-- Repeat the above, but for revenues_change
+INSERT INTO correlations
+SELECT 'revenues_change'::varchar AS measure,
+       corr(revenues_change, profits) AS profits,
+       corr(revenues_change, profits_change) AS profits_change,
+       corr(revenues_change, revenues_change) AS revenues_change
+  FROM fortune500;
+  
+--Select all rows and columns from the correlations table to view the correlation matrix.
+--First, you will need to round each correlation to 2 decimal places.
+--The output of corr() is of type double precision, so you will need to also cast columns to numeric.
+DROP TABLE IF EXISTS correlations;
+CREATE TEMP TABLE correlations AS
+SELECT 'profits'::varchar AS measure,
+       corr(profits, profits) AS profits,
+       corr(profits, profits_change) AS profits_change,
+       corr(profits, revenues_change) AS revenues_change
+  FROM fortune500;
+INSERT INTO correlations
+SELECT 'profits_change'::varchar AS measure,
+       corr(profits_change, profits) AS profits,
+       corr(profits_change, profits_change) AS profits_change,
+       corr(profits_change, revenues_change) AS revenues_change
+  FROM fortune500;
+INSERT INTO correlations
+SELECT 'revenues_change'::varchar AS measure,
+       corr(revenues_change, profits) AS profits,
+       corr(revenues_change, profits_change) AS profits_change,
+       corr(revenues_change, revenues_change) AS revenues_change
+  FROM fortune500;
+-- Select each column, rounding the correlations
+SELECT measure, 
+       round(profits::numeric, 2) AS profits,
+       round(profits_change::numeric, 2) AS profits_change,
+       round(revenues_change::numeric, 2) AS revenues_change
+  FROM correlations;
+  
+  
+  
+---						Chapter 3: 
+
 
 
 
