65987e4833c03cbc26362452e961cd4dfc76bcd5
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-26 15:48:15 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -539,7 +539,55 @@ SELECT street, count(*)
 --Which of the following is NOT an issue you see with the values of street?
 --Ans: 	There are sometimes extra spaces at the beginning and end of values
 
-	--
+	--Trimming
+SELECT distinct street,
+       -- Trim off unwanted characters from street
+       trim(street,'0123456789 #/.') AS cleaned_street
+  FROM evanston311
+ ORDER BY street;
+ 
+	--Exploring unstructured text
+--Use ILIKE to count rows in evanston311 where the description contains 'trash' or 'garbage' regardless of case.
+-- Count rows
+SELECT count(*)
+  FROM evanston311
+ -- Where description includes trash or garbage
+ WHERE description ILIKE '%trash%' 
+    OR description ILIKE '%garbage%';
+    
+    
+--category values are in title case. Use LIKE to find category values with 'Trash' or 'Garbage' in them.
+-- Select categories containing Trash or Garbage
+SELECT category
+  FROM evanston311
+ -- Use LIKE
+ WHERE category LIKE '%Trash%' 
+    OR category LIKE '%Garbage%';
+ 
+ --Count rows where the description includes 'trash' or 'garbage' but the category does not.
+-- Count rows
+SELECT count(*)
+  FROM evanston311 
+ -- description contains trash or garbage (any case)
+ WHERE (description iLIKE  '%trash%'
+    OR description iLIKE  '%garbage%') 
+ -- category does not contain Trash or Garbage
+   AND category NOT LIKE '%Trash%'
+   AND category NOT LIKE '%Garbage%';
+   
+--Find the most common categories for rows with a description about trash that don't have a trash-related category.
+-- Count rows with each category
+SELECT category, count(*)
+  FROM evanston311 
+ WHERE (description ILIKE '%trash%'
+    OR description ILIKE '%garbage%') 
+   AND category NOT LIKE '%Trash%'
+   AND category NOT LIKE '%Garbage%'
+ -- What are you counting?
+ GROUP BY category
+ --- order by most frequent values
+ ORDER BY Count desc
+ LIMIT 10;
 
 
 
