280a12de78f62ce59ce657ca1dc2949d05a2cb35
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-28 00:07:55 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -660,5 +660,95 @@ You'll also need to do some additional cleanup of a few cases that don't fit thi
 Then the evanston311 table can be joined to recode to group requests by the new standardized category values.
 */
 --Create recode with a standardized column; use split_part() and then rtrim() to remove any remaining whitespace on the result of split_part().
+-- Fill in the command below with the name of the temp table
+DROP TABLE IF EXISTS recode;
+
+-- Create and name the temporary table
+CREATE TEMP TABLE recode AS
+-- Write the select query to generate the table 
+-- with distinct values of category and standardized values
+  SELECT DISTINCT category, 
+         rtrim(split_part(category, '-', 1)) AS standardized
+    -- What table are you selecting the above values from?
+    FROM evanston311;
+-- Look at a few values before the next step
+SELECT DISTINCT standardized 
+  FROM recode
+ WHERE standardized LIKE 'Trash%Cart'
+    OR standardized LIKE 'Snow%Removal%';
+    
+--UPDATE standardized values LIKE 'Trash%Cart' to 'Trash Cart'. UPDATE standardized values of 'Snow Removal/Concerns' and 'Snow/Ice/Hazard Removal' to 'Snow Removal'.
+-- Code from previous step
+DROP TABLE IF EXISTS recode;
+CREATE TEMP TABLE recode AS
+  SELECT DISTINCT category, 
+         rtrim(split_part(category, '-', 1)) AS standardized
+    FROM evanston311;
+-- Update to group trash cart values
+UPDATE recode 
+   SET standardized='Trash Cart' 
+ WHERE standardized LIKE 'Trash%Cart';
+-- Update to group snow removal values
+UPDATE recode 
+   SET standardized='Snow Removal' 
+ WHERE standardized LIKE 'Snow%Removal%';
+-- Examine effect of updates
+SELECT DISTINCT standardized 
+  FROM recode
+ WHERE standardized LIKE 'Trash%Cart'
+    OR standardized LIKE 'Snow%Removal%';
+    
+--UPDATE recode by setting standardized values of 'THIS REQUEST IS INACTIVE...Trash Cart', 
+--'(DO NOT USE) Water Bill', 'DO NOT USE Trash', and 'NO LONGER IN USE' to 'UNUSED'.
+-- Code from previous step
+DROP TABLE IF EXISTS recode;
+CREATE TEMP TABLE recode AS
+  SELECT DISTINCT category, 
+         rtrim(split_part(category, '-', 1)) AS standardized
+    FROM evanston311;
+UPDATE recode SET standardized='Trash Cart' 
+ WHERE standardized LIKE 'Trash%Cart';
+UPDATE recode SET standardized='Snow Removal' 
+ WHERE standardized LIKE 'Snow%Removal%';
+-- Update to group unused/inactive values
+UPDATE recode 
+   SET standardized='UNUSED' 
+ WHERE standardized IN ('THIS REQUEST IS INACTIVE...Trash Cart',
+               '(DO NOT USE) Water Bill',
+               'DO NOT USE Trash', 
+               'NO LONGER IN USE' );
+-- Examine effect of updates
+SELECT DISTINCT standardized 
+  FROM recode
+ ORDER BY standardized;
+ 
+ --Now, join the evanston311 and recode tables to count the number of requests with each of the standardized values. List the most common standardized values first.
+-- Code from previous step
+DROP TABLE IF EXISTS recode;
+CREATE TEMP TABLE recode AS
+  SELECT DISTINCT category, 
+         rtrim(split_part(category, '-', 1)) AS standardized
+  FROM evanston311;
+UPDATE recode SET standardized='Trash Cart' 
+ WHERE standardized LIKE 'Trash%Cart';
+UPDATE recode SET standardized='Snow Removal' 
+ WHERE standardized LIKE 'Snow%Removal%';
+UPDATE recode SET standardized='UNUSED' 
+ WHERE standardized IN ('THIS REQUEST IS INACTIVE...Trash Cart', 
+               '(DO NOT USE) Water Bill',
+               'DO NOT USE Trash', 'NO LONGER IN USE');
+-- Select the recoded categories and the count of each
+SELECT standardized, count(*)
+-- From the original table and table with recoded values
+  FROM evanston311 
+       LEFT JOIN recode 
+       -- What column do they have in common?
+       ON evanston311.category = recode.category
+ -- What do you need to group by to count?
+ GROUP BY standardized
+ -- Display the most common val values first
+ ORDER BY count desc;
+ 
+ 		--
 
 
