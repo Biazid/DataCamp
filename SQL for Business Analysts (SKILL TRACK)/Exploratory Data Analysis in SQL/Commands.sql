eed61a255c5cde0db0c8aefb60119ce9b4511b1a
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-28 20:29:15 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -969,10 +969,32 @@ SELECT to_char(date_created, 'day') AS day,
  
  
  
-  
-  
-  
-  
+		  --Date truncation
+/*
+Unlike date_part() or EXTRACT(), date_trunc() keeps date/time units larger than the field you specify as part of the date. 
+So instead of just extracting one component of a timestamp, date_trunc() returns the specified unit and all larger ones as well.
+
+Recall the syntax:
+
+date_trunc('field', timestamp)
+
+Using date_trunc(), find the average number of Evanston 311 requests created per day for each month of the data. 
+Ignore days with no requests when taking the average.
+ */ 
+ --Write a subquery to count the number of requests created per day.
+--Select the month and average count per month from the daily_count subquery 
+  -- Aggregate daily counts by month
+SELECT date_trunc('month', day) AS month,
+       avg(count)
+  -- Subquery to compute daily counts
+  FROM (SELECT date_trunc('day', date_created) AS day,
+               count(*) AS count
+          FROM evanston311
+         GROUP BY day) AS daily_count
+ GROUP BY month
+ ORDER BY month;
+ 
+ 
   
   
   
