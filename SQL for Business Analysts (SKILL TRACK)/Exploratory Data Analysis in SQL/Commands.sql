18eca4ae14081f91648c49aed6fabb8c9582033d
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-28 20:17:02 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -955,7 +955,20 @@ DOW stands for "day of week."
 --Group by day (the name of the day of the week) and the integer value for the day of the week (use a function).
 --Order by the integer value of the day of the week using the same function used in GROUP BY.
   
-  
+ -- Select name of the day of the week the request was created 
+SELECT to_char(date_created, 'day') AS day, 
+       -- Select avg time between request creation and completion
+       avg(date_completed - date_created) AS duration
+  FROM evanston311 
+ -- Group by the name of the day of the week and 
+ -- integer value of day of week the request was created
+ GROUP BY day, EXTRACT(DOW FROM date_created)
+ -- Order by integer value of the day of the week 
+ -- the request was created
+ ORDER BY EXTRACT(DOW FROM date_created);
+ 
+ 
+ 
   
   
   
