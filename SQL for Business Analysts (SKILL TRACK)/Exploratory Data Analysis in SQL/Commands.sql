a274464b680fe071b20613b7ba30188d4626bd7e
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-29 11:30:57 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/Exploratory Data Analysis in SQL/Commands.sql
@@ -994,7 +994,141 @@ SELECT date_trunc('month', day) AS month,
  GROUP BY month
  ORDER BY month;
  
+ 	--	Find missing dates
+
+/*
+The generate_series() function can be useful for identifying missing dates.
+Recall:
+
+generate_series(from, to, interval)
+
+where from and to are dates or timestamps, and interval can be specified as a string with a number and a unit of time, such as '1 month'.
+Are there any days in the Evanston 311 data where no requests were created?
+*/
+--Write a subquery using generate_series() to get all dates between the min() and max() date_created in evanston311.
+--Write another subquery to select all values of date_created as dates from evanston311.
+--Both subqueries should produce values of type date (look for the ::).
+--Select dates (day) from the first subquery that are NOT IN the results of the second subquery. This gives you days that are not in date_created.
+SELECT day
+-- 1) Subquery to generate all dates
+-- from min to max date_created
+  FROM (SELECT generate_series(min(date_created),
+                               max(date_created),
+                               '1 day'::interval):: date AS day
+          -- What table is date_created in?
+          FROM evanston311) AS all_dates
+-- 4) Select dates (day from above) that are NOT IN the subquery
+ WHERE day not in  
+       -- 2) Subquery to select all date_created values as dates
+       (SELECT date_created::date
+          FROM evanston311);
+	  
+	  
+	  
+	  --	Custom aggregation periods
+	  /*
+Find the median number of Evanston 311 requests per day in each six month period from 2016-01-01 to 2018-06-30. Build the query following the three steps below.
+Recall that to aggregate data by non-standard date/time intervals, such as six months, you can use generate_series() to create bins with lower and upper bounds 
+of time, and then summarize observations that fall in each bin.
+*/
+  --1 --Use generate_series() to create bins of 6 month intervals. Recall that the upper bin values are exclusive, 
+  --so the values need to be one day greater than the last day to be included in the bin.
+--Notice how in the sample code, the first bin value of the upper bound is July 1st, and not June 30th.
+--Use the same approach when creating the last bin values of the lower and upper bounds (i.e. for 2018).
+  -- Generate 6 month bins covering 2016-01-01 to 2018-06-30
+
+-- Create lower bounds of bins
+SELECT generate_series('2016-01-01',  -- First bin lower value
+                       '2018-01-01',  -- Last bin lower value
+                       '6 month'::interval) AS lower,
+-- Create upper bounds of bins
+       generate_series('2016-07-01',  -- First bin upper value
+                       '2018-07-30',  -- Last bin upper value
+                       '6 month'::interval) AS upper
+
+--2--Count the number of requests created per day. Remember to not count *, or you will risk counting NULL values.
+--Include days with no requests by joining evanston311 to a daily series from 2016-01-01 to 2018-06-30.
+-- Note that because we are not generating bins, you can use June 30th as your series end date.
+  -- Count number of requests made per day 
+SELECT day, count(date_created) AS count
+-- Use a daily series from 2016-01-01 to 2018-06-30 
+-- to include days with no requests
+  FROM (SELECT generate_series('2016-01-01',  -- series start date
+                               '2018-06-30',  -- series end date
+                               '1 day'::interval)::date AS day) AS daily_series
+       LEFT JOIN evanston311
+       -- match day from above (which is a date) to date_created
+       ON day = date_created::date
+ GROUP BY day;
  
+ --3--Assign each daily count to a single 6 month bin by joining bins to daily_counts.
+--Compute the median value per bin using percentile_disc().
+  -- Bins from Step 1
+WITH bins AS (
+	 SELECT generate_series('2016-01-01',
+                            '2018-01-01',
+                            '6 months'::interval) AS lower,
+            generate_series('2016-07-01',
+                            '2018-07-01',
+                            '6 months'::interval) AS upper),
+-- Daily counts from Step 2
+     daily_counts AS (
+     SELECT day, count(date_created) AS count
+       FROM (SELECT generate_series('2016-01-01',
+                                    '2018-06-30',
+                                    '1 day'::interval)::date AS day) 
+                                                     AS daily_series
+            LEFT JOIN evanston311
+            ON day = date_created::date
+      GROUP BY day)
+-- Select bin bounds 
+SELECT lower, 
+       upper, 
+       -- Compute median of count for each bin
+       percentile_disc(0.5) WITHIN GROUP (ORDER BY day) 
+                                                        AS median
+  -- Join bins and daily_counts
+  FROM bins
+       LEFT JOIN daily_counts
+       -- Where the day is between the bin bounds
+       ON day >= lower
+          AND day < upper
+ -- Group by bin bounds
+ GROUP BY lower, upper
+ ORDER BY lower;
+ 
+ 
+ 			--Monthly average with missing dates
+/* Find the average number of Evanston 311 requests created per day for each month of the data.
+This time, do not ignore dates with no requests. */
+ -- Generate a series of dates from 2016-01-01 to 2018-06-30.
+--Join the series to a subquery to count the number of requests created per day.
+--Use date_trunc() to get months from date, which has all dates, NOT day.
+--Use coalesce() to replace NULL count values with 0. Compute the average of this value.
+  
+  -- generate series with all days from 2016-01-01 to 2018-06-30
+WITH all_days AS 
+     (SELECT generate_series('2016-01-01',
+                             '2018-06-30',
+                             '1 day'::interval) AS date),
+     -- Subquery to compute daily counts
+     daily_count AS 
+     (SELECT date_trunc('day', date_created) AS day,
+             count(*) AS count
+        FROM evanston311
+       GROUP BY day)
+-- Aggregate daily counts by month using date_trunc
+SELECT date_trunc('month', date) AS month,
+       -- Use coalesce to replace NULL count values with 0
+       avg(coalesce(count, 0)) AS average
+  FROM all_days
+       LEFT JOIN daily_count
+       -- Joining condition
+       ON all_days.date=daily_count.day
+ GROUP BY month
+ ORDER BY month; 
+ 
+ --			
   
   
   
