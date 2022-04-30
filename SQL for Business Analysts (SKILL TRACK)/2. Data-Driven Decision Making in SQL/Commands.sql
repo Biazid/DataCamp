66c1937ed3a1b88fceb644f77def202e25e788d1
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-04-30 23:08:03 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/2. Data-Driven Decision Making in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/2. Data-Driven Decision Making in SQL/Commands.sql
@@ -541,7 +541,90 @@ WHERE movie_id in
 		 FROM renting));
 		 	
 			
-			--
+			
+			--Analyzing customer behavior
+			
+			
+--A new advertising campaign is going to focus on customers who rented fewer than 5 movies. Use a correlated query to extract all customer information 
+--for the customers of interest.
+
+--1 First, count number of movie rentals for customer with customer_id=45. Give the table renting the alias r.
+
+-- Count movie rentals of customer 45
+select count(*)
+from renting r
+where r.customer_id=45;
+
+--2 Now select all columns from the customer table where the number of movie rentals is smaller than 5.
+
+-- Select customers with less than 5 movie rentals
+SELECT *
+FROM customers as c
+WHERE 5> 
+	(SELECT count(*)
+	FROM renting as r
+	WHERE r.customer_id = c.customer_id);
+	
+	
+	
+			--Customers who gave low ratings
+
+
+
+--Identify customers who were not satisfied with movies they watched on MovieNow. Report a list of customers with minimum rating smaller than 4.
+
+--1 Calculate the minimum rating of customer with ID 7.
+
+
+-- Calculate the minimum rating of customer with ID 7
+SELECT min(rating)
+from renting
+where customer_id=7;
+
+--2 Select all customers with a minimum rating smaller than 4. Use the first letter of the table as an alias.
+
+SELECT *
+FROM customers c
+WHERE 4 > 
+	(SELECT MIN(rating)
+	FROM renting AS r
+	WHERE r.customer_id = c.customer_id);
+	
+		
+		
+		--Movies and ratings with correlated queries
+
+
+
+--Report a list of movies that received the most attention on the movie platform, (i.e. report all movies with more than 5 ratings and all movies 
+--with an average rating higher than 8).
+
+--1 Select all movies with more than 5 ratings. Use the first letter of the table as an alias.
+
+SELECT *
+FROM movies m
+WHERE 5 < -- Select all movies with more than 5 ratings
+	(SELECT count(rating)
+	FROM renting r
+	WHERE r.movie_id=m.movie_id);
+	
+--2 Select all movies with an average rating higher than 8.
+
+SELECT *
+FROM movies AS m
+WHERE 8 < 
+	(SELECT avg(rating)
+	FROM renting AS r
+	WHERE r.movie_id = m.movie_id);
+
+
+
+				--
+				
+				
+-- 
+
+
 
 
 
