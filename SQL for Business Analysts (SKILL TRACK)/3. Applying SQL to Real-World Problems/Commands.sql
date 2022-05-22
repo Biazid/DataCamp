35f9583b3a1f0b10765be0034d6bcdf68dbc36ce
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-22 17:57:19 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/3. Applying SQL to Real-World Problems/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/3. Applying SQL to Real-World Problems/Commands.sql
@@ -85,8 +85,65 @@ SELECT payment_date,
   EXTRACT(hour from payment_date) AS payment_hour 
 FROM payment;
 
-                           --
+                           --Aggregating finances
+
+--In this exercise you would like to learn more about the differences in payments between the customers who are active and those who are not.
+
+--Find out the differences in their total number of payments by COUNT()ing the payment_id.
+--Find out the differences in their average payments by using AVG().
+--Find out the differences in their total payments by using SUM().
+--Ensure the aggregate functions GROUP BY whether customer payments are active.
+
+SELECT active, 
+       count(payment_id) AS num_transactions, 
+       avg(amount) AS avg_amount, 
+       sum(amount) AS total_amount
+FROM payment AS p
+INNER JOIN customer AS c
+  ON p.customer_id = c.customer_id
+GROUP BY active;
+
+		
+				--Aggregating strings
+--You are planning to update your storefront window to demonstrate how family-friendly and multi-lingual your DVD collection is. 
+--To prepare for this you need to prepare a comma-separated list G-rated film titles by language released in 2010.
+
+--Return a column with a list of comma-separated film titles by using the STRING_AGG() function.
+--Limit the results to films that have a release_year of 2010 AND have a rating of 'G'.
+--Ensure that the operation is grouped by the language name.
+
+SELECT name, 
+	STRING_AGG(title, ',') AS film_titles
+FROM film AS f
+INNER JOIN language AS l
+  ON f.language_id = l.language_id
+WHERE release_year = 2010
+  AND rating = 'G'
+GROUP BY name;
 
 
 
+					--Which tables?
+/*
+You must answer the following question using a query:
+
+Which films are most frequently rented?
+
+Which of these table(s) do you need to prepare the query to answer this question?
+
+Tables	
+actor		film_actor
+address		inventory
+category	language
+customer	payment
+film		rental
+
+Ans: I need to explore my database to find the data I'm looking for!
+*/
+
+
+
+							--Chapter 2: Find Your Data
+
+--
 
