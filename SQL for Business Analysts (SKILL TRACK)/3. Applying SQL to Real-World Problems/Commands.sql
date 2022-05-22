db6aa70533070f33ce1903fa277175825fac2edd
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-22 17:32:31 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/3. Applying SQL to Real-World Problems/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/3. Applying SQL to Real-World Problems/Commands.sql
@@ -1,1 +1,14 @@
+ --                               Chapter 1: Use Real-World SQL
+
+
+        --Review the essentials
+
+--In this exercise you must prepare the data you need (title & description) to run a promotion for the store's Italian & French language films from 2005.
+
+--1 Return the title and description from the film table. Make sure to alias this table AS f.
+
+SELECT title, description
+FROM film AS f
+
+--2 Use an INNER JOIN to bring in language data about your films using the language_id as the join column.
 
