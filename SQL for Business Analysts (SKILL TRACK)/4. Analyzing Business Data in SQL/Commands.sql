94f89a965536ec151c0a069f9d390437c145cb03
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-23 09:45:30 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/4. Analyzing Business Data in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/4. Analyzing Business Data in SQL/Commands.sql
@@ -1,1 +1,16 @@
+                                                                --Chapter 1: Revenue, cost, and profit
+                          --Revenue per customer
+/*                          
+You've been hired at Delivr as a data analyst! A customer just called Delivr's Customer Support team; she wants to double-check whether her receipts add up. 
+Going by her receipts, she calculated that her total bill on Delivr is $271, and she wants to make sure of that. Her user ID is 15.
+Help the Customer Support team by calculating her total bill! Sum up everything she's spent on Delivr orders; in other words, calculate the total revenue that Delivr 
+has generated from her.
+*/
+--Write the expression for revenue.
+--Keep only the records of user ID 15.
+
+
+
+
+
 
