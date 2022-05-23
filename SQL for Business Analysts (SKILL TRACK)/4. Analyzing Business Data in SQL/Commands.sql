75ee69b8fd2ab9c531d33d280d5cc5de4d3beaef
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-23 14:39:34 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/4. Analyzing Business Data in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/4. Analyzing Business Data in SQL/Commands.sql
@@ -9,6 +9,128 @@ has generated from her.
 --Write the expression for revenue.
 --Keep only the records of user ID 15.
 
+-- Calculate revenue
+SELECT sum(order_quantity*meal_price) AS revenue
+  FROM meals
+  JOIN orders 
+  ON meals.meal_id = orders.meal_id
+-- Keep only the records of customer ID 15
+WHERE user_id=15;
+
+
+                         --Revenue per week
+/*
+Delivr's first full month of operations was June 2018. At launch, the Marketing team ran an ad campaign on popular food channels on TV, with the number of ads 
+increasing each week through the end of the month. The Head of Marketing asks you to help her assess that campaign's success.
+
+Get the revenue per week for each week in June and check whether there's any consistent growth in revenue.
+
+Note: Don't be surprised if you get a date in May in the result. DATE_TRUNC('week', '2018-06-02') returns '2018-05-28', since '2018-06-02' is a Saturday and 
+the preceding Monday is on '2018-05-28'.
+*/
+
+--Write the expression for revenue.
+--Keep only the records of June 2018.
+
+SELECT DATE_TRUNC('week', order_date) :: DATE AS delivr_week,
+       -- Calculate revenue
+       SUM(meal_price*order_quantity) AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+-- Keep only the records in June 2018
+WHERE order_date BETWEEN '2018-06-01' AND '2018-06-30'
+GROUP BY delivr_week
+ORDER BY delivr_week ASC;
+
+
+                            --Total cost
+--What is Delivr's total cost since it began operating?
+--Ans: 92133
+
+      
+                            --Top meals by cost
+/*
+Alice from Finance wants to know what Delivr's top 5 meals are by overall cost; in other words, Alice wants to know the 5 meals that Delivr has spent 
+the most on for stocking.
+You're provided with an aggregate query; you'll need to fill in the blanks to get the output Alice needs.
+Note: Recall that in the meals table, meal_price is what the user pays Delivr for the meal, while meal_cost is what Delivr pays its eateries to stock this meal.
+*/
+
+--Calculate cost per meal ID.
+--Set the LIMIT to 5.
+
+SELECT
+  -- Calculate cost per meal ID
+  meals.meal_id,
+  sum(meal_cost*stocked_quantity) AS cost
+FROM meals
+JOIN stock ON meals.meal_id = stock.meal_id
+GROUP BY meals.meal_id
+ORDER BY cost DESC
+-- Only the top 5 meal IDs by purchase cost
+LIMIT 5;
+
+
+                            --Using CTEs
+/*
+Alice wants to know how much Delivr spent per month on average during its early months (before September 2018). 
+You'll need to write two queries to solve this problem:
+A query to calculate cost per month, wrapped in a CTE,
+A query that averages monthly cost before September 2018 by referencing the CTE.
+*/
+
+--1 Calculate cost per month.
+
+SELECT
+  -- Calculate cost
+  DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
+  sum(meal_cost*stocked_quantity) AS cost
+FROM meals
+JOIN stock ON meals.meal_id = stock.meal_id
+GROUP BY delivr_month
+ORDER BY delivr_month ASC;
+
+--2 Wrap the query you just wrote in a CTE named monthly_cost.
+
+WITH monthly_cost AS (
+  SELECT
+    DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
+    SUM(meal_cost * stocked_quantity) AS cost
+  FROM meals
+  JOIN stock ON meals.meal_id = stock.meal_id
+  GROUP BY delivr_month)
+
+SELECT *
+FROM monthly_cost;
+
+--3 Now that you've set up the monthly_cost CTE, find the average cost incurred before September 2018.
+
+-- Declare a CTE named monthly_cost
+WITH monthly_cost AS (
+  SELECT
+    DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
+    SUM(meal_cost * stocked_quantity) AS cost
+  FROM meals
+  JOIN stock ON meals.meal_id = stock.meal_id
+  GROUP BY delivr_month)
+
+SELECT
+  -- Calculate the average monthly cost before September
+  AVG(cost)
+FROM monthly_cost
+WHERE delivr_month<'2018-09-01';
+
+
+                                    --
+
+
+
+
+
+
+
+
+
 
 
 
