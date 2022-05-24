dda44c4bd772e9eb3049d8daad4329557ffadd29
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-24 21:09:36 +0600

    Update Commands.sql

--- SQL for Business Analysts (SKILL TRACK)/4. Analyzing Business Data in SQL/Commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/4. Analyzing Business Data in SQL/Commands.sql
@@ -931,7 +931,27 @@ Pivot the user revenues by month query so that the user ID is a row and each mon
 --Enable CROSSTAB() from tablefunc.
 --Declare the new pivot table's columns, user ID and the first three months of operation.
 
+-- Import tablefunc
+CREATE EXTENSION IF NOT EXISTS tablefunc;
 
+SELECT * FROM CROSSTAB($$
+  SELECT
+    user_id,
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    SUM(meal_price * order_quantity) :: FLOAT AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+ WHERE user_id IN (0, 1, 2, 3, 4)
+   AND order_date < '2018-09-01'
+ GROUP BY user_id, delivr_month
+ ORDER BY user_id, delivr_month;
+$$)
+-- Select user ID and the months from June to August 2018
+AS ct (user_id INT,
+       "2018-06-01" FLOAT,
+       "2018-07-01" FLOAT,
+       "2018-08-01" FLOAT)
+ORDER BY user_id ASC;
 
 
 
@@ -943,14 +963,49 @@ First, write a query to get the total costs by eatery in November and December 2
 
 Note: Recall from Chapter 1 that total cost is the sum of each meal's cost times its stocking quantity.
 */
+
 --1 Select the eatery and calculate total cost per eatery.
 --Keep only the records after October 2018.
 
+SELECT
+  -- Select eatery and calculate total cost
+  eatery,
+  DATE_TRUNC('month', stocking_date) :: DATE AS delivr_month,
+  SUM(meal_cost * stocked_quantity) :: FLOAT AS cost
+FROM meals
+JOIN stock ON meals.meal_id = stock.meal_id
+-- Keep only the records after October 2018
+WHERE stocking_date>'2018-10-30'
+GROUP BY eatery, delivr_month
+ORDER BY eatery, delivr_month;
 
 
+--2 Enable CROSSTAB from tablefunc.
+--Declare the new pivot table's columns, the eatery and the last two months of operation.
 
---2
 
+-- Import tablefunc
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    -- Select eatery and calculate total cost
+    eatery,
+    DATE_TRUNC('month', stocking_date) :: DATE AS delivr_month,
+    SUM(meal_cost * stocked_quantity) :: FLOAT AS cost
+  FROM meals
+  JOIN stock ON meals.meal_id = stock.meal_id
+  -- Keep only the records after October 2018
+  WHERE DATE_TRUNC('month', stocking_date) > '2018-10-01'
+  GROUP BY eatery, delivr_month
+  ORDER BY eatery, delivr_month;
+$$)
+
+-- Select the eatery and November and December 2018 as columns
+AS ct (eatery TEXT,
+       "2018-11-01" FLOAT,
+       "2018-12-01" FLOAT)
+ORDER BY eatery ASC;
 
 
 						--Report readability
