d01a5ffe8fd558d90a4dd92343f33f111d4d3fa9
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-25 20:22:44 +0600

    Update commands.sql

--- SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
@@ -523,8 +523,116 @@ In this exercise, use the subquery approach.
 --Setup a subquery that outputs all athletes age 16 or below.
 --Add a WHERE statement that references the subquery to filter for athletes age 16 or below.
 
+-- Pull summer bronze_medals, silver_medals, and gold_medals
+SELECT 
+	sum(bronze) as bronze_medals, 
+    sum(silver) as silver_medals, 
+    sum(gold) as gold_medals
+FROM summer_games
+-- Add the WHERE statement below
+WHERE athlete_id IN
+    -- Create subquery list for athlete_ids age 16 or below    
+    (SELECT id
+     FROM athletes
+     WHERE age<=16);
+     
+     
+     
+     				--Report 2: Top athletes in nobel-prized countries
+/*
+It's time to bring together all the concepts brought up in this chapter to expand on your dashboard! 
+Your next report to build is Report 2: Athletes Representing Nobel-Prize Winning Countries.
+
+Report Details:
+
+Column 1 should be event, which represents the Olympic event. Both summer and winter events should be included.
+Column 2 should be gender, which represents the gender of athletes in the event.
+Column 3 should be athletes, which represents the unique athletes in the event.
+Athletes from countries that have had no nobel_prize_winners should be excluded.
+The report should contain 10 events, where events with the most athletes show at the top.
+*/
+
+--1 Select event from the summer_games table and create the athletes field by counting the distinct number of athlete_id.
+
+-- Pull event and unique athletes from summer_games 
+SELECT 	
+	event,
+	COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+GROUP BY event;
+
+--2 Explore the event field to determine how to split up events by gender (without adding a join), 
+--then add the gender field by using a CASE statement that specifies a conditional for 'female' events (all other events should output as 'male').
+
+-- Pull event and unique athletes from summer_games 
+SELECT 
+	event, 
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female'
+    ELSE 'male' 
+    END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+GROUP BY event;
+
+--3 Filter for Nobel-prize-winning countries by creating a subquery that outputs country_id from the country_stats table for any country
+--with more than zero nobel_prize_winners.
+
+-- Pull event and unique athletes from summer_games 
+SELECT 
+    event,
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female' 
+    ELSE 'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+-- Only include countries that won a nobel prize
+WHERE country_id IN 
+	(SELECT country_id
+    FROM country_stats
+    WHERE nobel_prize_winners>0)
+GROUP BY event;
+
+--4 Copy your query with summer_games replaced by winter_games, UNION the two tables together, and order the final report to show the 10 rows with the most athletes.
+
+-- Pull event and unique athletes from summer_games 
+SELECT 
+    event,
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female' 
+    ELSE 'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+-- Only include countries that won a nobel prize
+WHERE country_id IN 
+	(SELECT country_id 
+    FROM country_stats 
+    WHERE nobel_prize_winners > 0)
+GROUP BY event
+-- Add the second query below and combine with a UNION
+UNION ALL
+SELECT 
+    event,
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female' 
+    ELSE 'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM winter_games
+-- Only include countries that won a nobel prize
+WHERE country_id IN 
+	(SELECT country_id 
+    FROM country_stats 
+    WHERE nobel_prize_winners > 0)
+GROUP BY event
+-- Order and limit the final output
+ORDER BY athletes DESC
+LIMIT 10;
 
 
+
+
+								--Chapter 3: Cleaning & Validation
+					--
 
 
 
