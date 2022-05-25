9171828b074665c5130902786f07150926b8cccb
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-25 20:34:45 +0600

    Update commands.sql

--- SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
@@ -632,14 +632,44 @@ LIMIT 10;
 
 
 								--Chapter 3: Cleaning & Validation
-					--
+								
+					--Identifying data types
+/*
+Being able to identify data types before setting up your query can help you plan for any potential issues. There is a group of tables, or a schema, 
+called the information_schema, which provides a wide array of information about the database itself, including the structure of tables and columns.
+
+The columns table houses useful details about the columns, including the data type.
+
+Note that the information_schema is not the default schema SQL looks at when querying, which means you will need to explicitly tell SQL to pull from this schema. 
+To pull a table from a non-default schema, use the syntax schema_name.table_name.
+*/
 
 
+--1 Select the fields column_name and data_type from the table columns that resides within the information_schema schema.
+--Filter only for the 'country_stats' table.
+
+-- Pull column_name & data_type from the columns table
+SELECT 
+	column_name,
+    data_type
+FROM information_schema.columns 
+-- Filter for the table 'country_stats'
+WHERE table_name = 'country_stats';
+
+--2 What is the data type of the year column?
+--Ans: character varying
 
 
+					--Interpreting error messages
+/*
+Inevitably, you will run into errors when running SQL queries. 
+It is important to understand how to interpret these errors to correctly identify what type of errorit is.
 
-
-
+The console contains two separate queries, each which will output an error when ran. In this exercise, you will run each query, read the error message, 
+and troubleshoot the error.
+*/
+--1 Run the query in the console.
+--After reading the error, fix it by converting the data type to float.
 
 
 
