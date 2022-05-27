02ec47b8d273e5df2483e2b60b91fb6e8f59dae3
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-27 21:47:43 +0600

    Update commands.sql

--- SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
@@ -1092,6 +1092,62 @@ ORDER BY medals DESC;
 
 --3 Notice the repeated values in the results. Add a column on the newest join statement to remove this duplication, 
 --and if you run into an error when trying to join, update the query so both fields are stored as type date.
+SELECT 
+	c.country,
+    -- Pull in pop_in_millions and medals_per_million 
+	pop_in_millions,
+    -- Add the three medal fields using one sum function
+	SUM(COALESCE(bronze,0) + COALESCE(silver,0) + COALESCE(gold,0)) AS medals,
+	SUM(COALESCE(bronze,0) + COALESCE(silver,0) + COALESCE(gold,0)) / CAST(cs.pop_in_millions AS float) AS medals_per_million
+FROM summer_games AS s
+JOIN countries AS c 
+ON s.country_id = c.id
+-- Update the newest join statement to remove duplication
+JOIN country_stats AS cs 
+ON s.country_id = cs.country_id AND s.year=CAST(cs.year AS DATE)
+GROUP BY c.country, pop_in_millions
+ORDER BY medals DESC;
+
+--4 Update country to country_code by trimming leading spaces, converting to upper case, removing . characters, and keeping only the left 3 characters, 
+--then filter out null populations and keep only the 25 rows with the most medals_per_million.
+
+SELECT 
+	-- Clean the country field to only show country_code
+    LEFT(REPLACE(UPPER(TRIM(c.country)), '.', ''), 3) AS country_code,
+    -- Pull in pop_in_millions and medals_per_million 
+	pop_in_millions,
+    -- Add the three medal fields using one sum function
+	SUM(COALESCE(bronze,0) + COALESCE(silver,0) + COALESCE(gold,0)) AS medals,
+	SUM(COALESCE(bronze,0) + COALESCE(silver,0) + COALESCE(gold,0)) / CAST(cs.pop_in_millions AS float) AS medals_per_million
+FROM summer_games AS s
+JOIN countries AS c 
+ON s.country_id = c.id
+-- Update the newest join statement to remove duplication
+JOIN country_stats AS cs 
+ON s.country_id = cs.country_id AND s.year = CAST(cs.year AS date)
+-- Filter out null populations
+WHERE cs.pop_in_millions IS NOT NULL
+GROUP BY c.country, pop_in_millions
+-- Keep only the top 25 medals_per_million rows
+ORDER BY medals_per_million DESC
+LIMIT 25;
+
+
+							
+							
+							--Chapter 4: Complex Calculations
+					--
+
+
+
+
+
+
+
+
+
+
+
 
 
 
