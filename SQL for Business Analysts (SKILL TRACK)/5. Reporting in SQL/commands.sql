d7d45ca046a2b7af25952449fb13ec3684296743
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-28 12:48:37 +0600

    Update commands.sql

--- SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
+++ SQL for Business Analysts (SKILL TRACK)/5. Reporting in SQL/commands.sql
@@ -1540,6 +1540,160 @@ where N is the number of rows to look back when doing the calculation. Remember 
 
 --1 Show daily_views and weekly_avg by date, where weekly_avg is the rolling 7 day average of views.
 
+SELECT
+	-- Pull in date and daily_views
+	date,
+	SUM(views) AS daily_views,
+    -- Calculate the rolling 7 day average
+	AVG(SUM(views)) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg
+FROM web_data
+GROUP BY date;
+
+--2 Alias the query as subquery, add an outer layer that pulls date and weekly_avg, and order by date in descending order.
+--Create the field weekly_avg_previous that takes the weekly_avg from 7 days prior to the given date.
+
+SELECT 
+	-- Pull in date and weekly_avg
+	date,
+  weekly_avg,
+  -- Output the value of weekly_avg from 7 days prior
+  LAG(weekly_avg,7) OVER (ORDER BY date) AS weekly_avg_previous
+FROM
+  (SELECT
+      -- Pull in date and daily_views
+      date,
+      SUM(views) AS daily_views,
+      -- Calculate the rolling 7 day average
+      AVG(SUM(views)) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg
+  FROM web_data
+  -- Alias as subquery
+  GROUP BY date) AS subquery
+-- Order by date in descending order
+ORDER BY date DESC;
+
+
+--3 Calculate perc_change where a value of 0 indicates no change, a negative value represents a drop in views versus previous period, and a positive value 
+--represents growth.
+
+
+SELECT 
+	-- Pull in date and weekly_avg
+	date,
+    weekly_avg,
+    -- Output the value of weekly_avg from 7 days prior
+    LAG(weekly_avg,7) OVER (ORDER BY date) AS weekly_avg_previous,
+    -- Calculate percent change vs previous period
+    weekly_avg / LAG(weekly_avg,7) OVER (ORDER BY date) - 1 as perc_change
+FROM
+  (SELECT
+      -- Pull in date and daily_views
+      date,
+      SUM(views) AS daily_views,
+      -- Calculate the rolling 7 day average
+      AVG(SUM(views)) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg
+  FROM web_data
+  -- Alias as subquery
+  GROUP BY date) AS subquery
+-- Order by date in descending order
+ORDER BY date DESC;
+
+
+					--Report 4: Tallest athletes and % GDP by region
+/*
+The final report on the dashboard is Report 4: Avg Tallest Athlete and % of world GDP by Region.
+
+Report Details:
+
+Column 1 should be region found in the countries table.
+Column 2 should be avg_tallest, which averages the tallest athlete from each country within the region.
+Column 3 should be perc_world_gdp, which represents what % of the world's GDP is attributed to the region.
+Only winter_games should be included (no summer events).
+*/
+
+--1 Pull country_id and height for winter athletes, group by these two fields, and order by country_id and height in descending order.
+--Use ROW_NUMBER() to create row_num, which numbers rows within a country by height where 1 is the tallest.
+
+SELECT 
+	-- Pull in country_id and height
+	country_id,
+    height,
+    -- Number the height of each country's athletes
+    ROW_NUMBER() OVER(PARTITION BY country_id ORDER BY height DESC) AS row_num
+FROM winter_games AS w
+JOIN athletes AS a
+ON a.id=w.athlete_id
+GROUP BY country_id, height
+-- Order by country_id and then height in descending order
+ORDER BY country_id, height DESC;
+
+
+--2 Alias your query as subquery then use this subquery to join the countries table to pull in the region and average_tallest field, 
+--the last of which averages the tallest height of each country.
+
+SELECT
+	-- Pull in region and calculate avg tallest height
+	region,
+    AVG(height) AS avg_tallest
+FROM countries AS c
+JOIN
+    (SELECT 
+   	    -- Pull in country_id and height
+        country_id, 
+        height, 
+        -- Number the height of each country's athletes
+        ROW_NUMBER() OVER (PARTITION BY country_id ORDER BY height DESC) AS row_num
+    FROM winter_games AS w 
+    JOIN athletes AS a 
+    ON w.athlete_id = a.id
+    GROUP BY country_id, height
+    -- Alias as subquery
+    ORDER BY country_id, height DESC) AS subquery
+ON c.id=subquery.country_id
+-- Only include the tallest height for each country
+WHERE row_num = 1
+GROUP BY region;
+
+
+--3 Join to the country_stats table to create the perc_world_gdp field that calculates [region's GDP] / [world's GDP].
+
+
+SELECT
+	-- Pull in region and calculate avg tallest height
+    region,
+    AVG(height) AS avg_tallest,
+    -- Calculate region's percent of world gdp
+    SUM(gdp)/SUM(SUM(gdp)) OVER () AS perc_world_gdp    
+FROM countries AS c
+JOIN
+    (SELECT 
+     	-- Pull in country_id and height
+        country_id, 
+        height, 
+        -- Number the height of each country's athletes
+        ROW_NUMBER() OVER (PARTITION BY country_id ORDER BY height DESC) AS row_num
+    FROM winter_games AS w 
+    JOIN athletes AS a ON w.athlete_id = a.id
+    GROUP BY country_id, height
+    -- Alias as subquery
+    ORDER BY country_id, height DESC) AS subquery
+ON c.id = subquery.country_id
+-- Join to country_stats
+JOIN country_stats AS cs 
+ON cs.country_id = c.id
+-- Only include the tallest height for each country
+WHERE row_num = 1
+GROUP BY region;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
