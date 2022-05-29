14614a47a2ae396fd6cc7a055dac2fa080eace92
MD Biazid Bostame <biazid.bostame05@gmail.com>
2022-05-29 16:02:52 +0600

    Update commands.sql

--- Cleaning Data in SQL Server Databases/commands.sql
+++ Cleaning Data in SQL Server Databases/commands.sql
@@ -809,12 +809,91 @@ Ans: 2
 
 
 
-					--
+					--PIVOT or UNPIVOT?
+/*
+In this lesson, you learned the differences between using PIVOT and UNPIVOT.
+
+Which of the following statements about these relational operators is false?
+
+1. PIVOT turns the unique values from one column into multiple columns in the output.
+2. UNPIVOT turns columns into rows. 
+3. UNPIVOT turns the unique values from one column into multiple columns in the output.    Ans
+4. Within the PIVOT operation you have to use an aggregate function.
+*/
 
 
+					--Turning rows into columns
+/*
+In this lesson, you learned that PIVOT turns the unique values from one column into multiple columns.
+
+Analyzing the data of paper_shop_monthly_sales, you realize the structure of this table is not appropriate for the report that you want to make.
+
+You want to generate a report with this appearance:
+
+|year_of_sale|notebooks|pencils|crayons|
+|------------|---------|-------|-------|
+| 2018       | 150     | 150   | 80    |
+| 2019       | 230     | 130   | 170   |
+In other words, you want to change the data you have in the rows to data into columns, and sum the units for every year.
+
+As you learned from the previous exercises, the name of the products and the units has to be split. This is done in the subselect, take a look at it.
+*/
+
+--Select the pivoted columns for every product.
+--Include the sum of the units inside the PIVOT operator.
+--After the FOR statement, include the name of the column that contains the values that will become column headers.
+--Give to the PIVOT operator the name paper_shop_pivot.
+
+SELECT
+	year_of_sale,
+    -- Select the pivoted columns
+	notebooks, 
+	pencils, 
+	crayons
+FROM
+   (SELECT 
+		SUBSTRING(product_name_units, 1, charindex('-', product_name_units)-1) product_name, 
+		CAST(SUBSTRING(product_name_units, charindex('-', product_name_units)+1, len(product_name_units)) AS INT) units,	
+		year_of_sale
+	FROM paper_shop_monthly_sales) sales
+-- Sum the units for column that contains the values that will be column headers
+PIVOT (SUM(units) FOR product_name IN (notebooks, pencils, crayons))
+-- Give the alias name
+AS paper_shop_pivot
 
 
+					--Turning columns into rows
+/*
+In the previous exercise, you turned the names of the products you had in the rows into columns, and then you summarized the units of the products for every year.
 
+Suppose you stored the result from the previous exercise in a new table called pivot_sales, and now you want to turn the columns notebooks, pencils,
+and crayons into row values.
+
+The expected result will be:
+
+| year_of_sale | units | product_name |
+|--------------|-------|--------------|
+| 2018         | 150   | notebooks    |
+| 2018         | 150   | pencils      |
+| 2018         | 80    | crayons      |
+| 2019         | 230   | notebooks    |
+| 2019         | 130   | pencils      |
+| 2019         | 170   | crayons      |
+
+*/
+
+--Use the appropriate operator to convert columns into rows.
+--Write the name of the resulting column that will contain the turned columns.
+--Write the names of the columns you want to turn into rows.
+--Give to the UNPIVOT operator the alias unpivot_sales.
+
+SELECT * FROM pivot_sales
+-- Use the operator to convert columns into rows
+UNPIVOT
+	-- The resulting column that will contain the turned columns into rows
+	(units FOR product_name IN (notebooks, pencils, crayons))
+-- Give the alias name
+AS unpivot_sales
 
 
 
